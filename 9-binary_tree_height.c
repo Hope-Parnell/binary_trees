4a3b50093adf2dd363f644fd76ad7fe13974e18a
Hope-Parnell <parnell.hope.c@gmail.com>
2022-04-04 12:56:28 -0500

    task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - returns the height of a binary tree
+ * @tree: pointer to root
+ *
+ * Return: height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (!tree)
+		return (0);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+	return (left > right ? left : right);
+}
