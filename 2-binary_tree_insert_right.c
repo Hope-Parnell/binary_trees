5c7d79423bf7f5c985d0a33ec2c9d08611655b93
Hope-Parnell <parnell.hope.c@gmail.com>
2022-04-04 11:23:16 -0500

    task 2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - creates a binary tree node as the right child
+ * @parent: the parent node
+ * @value: value to store in the new node
+ *
+ * Return: the newly created node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (!parent)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
+		return (NULL);
+
+	new->parent = parent;
+	new->n = value;
+	new->right = NULL;
+	new->right = NULL;
+	if (parent->right)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+	return (new);
+}
