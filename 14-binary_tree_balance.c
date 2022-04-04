a5b32582354c7c8b60e2d34e30160970570289a3
Hope-Parnell <parnell.hope.c@gmail.com>
2022-04-04 14:10:26 -0500

    task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ *
+ *
+ *
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (height(tree->left) - height(tree->right));
+}
+
+
+/**
+ * height - returns the height of a binary tree
+ * @tree: pointer to root
+ *
+ * Return: height of the tree including root
+ */
+
+size_t height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (!tree)
+		return (0);
+	left = 1 + height(tree->left);
+	right = 1 + height(tree->right);
+	return (left > right ? left : right);
+}
