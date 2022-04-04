50636412b937612210dab2cd7dcbc67541e9cd84
Hope-Parnell <parnell.hope.c@gmail.com>
2022-04-04 15:11:19 -0500

    task 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *p;
+
+	if (node == NULL || !node->parent)
+		return (NULL);
+	p = node->parent;
+	if (p->left == node)
+		return (p->right);
+	return (p->left);
+}
